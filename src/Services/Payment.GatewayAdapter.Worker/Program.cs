a3d67cb13747eb32ac934d1035836513fe90c4e3
Chuma <chuxnwuba@gmail.com>
2026-01-14 00:10:21 -0500

    Gateway worker setup

--- src/Services/Payment.GatewayAdapter.Worker/Program.cs
+++ src/Services/Payment.GatewayAdapter.Worker/Program.cs
@@ -1,2 +1,29 @@
-﻿Console.WriteLine("Worker starting...");
-await Task.Delay(-1);
+﻿using Contracts;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Worker starting...");
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<PaymentRequestedConsumer>();
+
+            x.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(context.Configuration["RabbitMQ:Host"], "/", h =>
+                {
+                    h.Username(context.Configuration["RabbitMQ:Username"] ?? "guest");
+                    h.Password(context.Configuration["RabbitMQ:Password"] ?? "guest");
+                });
+
+                cfg.ReceiveEndpoint("payment_adapter_queue", e =>
+                {
+                    e.ConfigureConsumer<PaymentRequestedConsumer>(ctx);
+                });
+            });
+        });
+    });
